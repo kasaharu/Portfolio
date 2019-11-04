95ccea4f8d64d86a768436e93842d1c2fcab7b31
Wataru.Kasahara <kasahara1019@gmail.com>
2019-11-04 14:33:27 +0900

    '/blogs' にアクセスしたときに BlogsComponent を表示

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogsComponent } from './feature/blogs/blogs.component';
 import { TopComponent } from './feature/top/top.component';
 
-const routes: Routes = [{ path: '', component: TopComponent }];
+const routes: Routes = [{ path: '', component: TopComponent }, { path: 'blogs', component: BlogsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
