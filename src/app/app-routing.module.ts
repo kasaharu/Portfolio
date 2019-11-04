3f2da3bb97929fe5d7bd81f77578d64d4181fa31
Wataru.Kasahara <kasahara1019@gmail.com>
2019-11-04 13:47:54 +0900

    '/' にアクセスしたときに TopComponent を表示

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TopComponent } from './feature/top/top.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: TopComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
