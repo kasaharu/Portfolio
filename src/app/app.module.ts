117c727c99b5dbd6aac8aa70b2fa8bea7ac3cf59
Wataru.Kasahara <kasahara1019@gmail.com>
2019-11-04 15:20:31 +0900

    ActivityComponent の実装

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivityComponent } from './feature/activity/activity.component';
 import { BlogsComponent } from './feature/blogs/blogs.component';
 import { HeaderComponent } from './feature/header/header.component';
 import { TopComponent } from './feature/top/top.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, TopComponent, BlogsComponent],
+  declarations: [AppComponent, HeaderComponent, TopComponent, BlogsComponent, ActivityComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/feature/activity/activity.component.ts
+++ src/app/feature/activity/activity.component.ts
@@ -1,5 +1,15 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
+export interface Slide {
+  serviceName: string;
+  url: string;
+}
+
+export interface Sns {
+  serviceName: string;
+  url: string;
+}
+
 @Component({
   selector: 'app-activity',
   templateUrl: './activity.component.html',
@@ -7,10 +17,13 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ActivityComponent implements OnInit {
+  constructor() {}
+  readonly slide: Slide = { serviceName: 'Speaker Deck', url: 'https://speakerdeck.com/kasaharu' };
+  readonly snsList: Sns[] = [
+    { serviceName: 'GitHub', url: 'https://github.com/kasaharu' },
+    { serviceName: 'Twitter', url: 'https://twitter.com/kasaharu' },
+    { serviceName: 'connpass', url: 'https://connpass.com/user/kasaharu/' },
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
