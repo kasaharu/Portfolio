972b4d65d7782467fe314887e0f2e2e0ed90178f
Wataru.Kasahara <kasahara1019@gmail.com>
2019-11-04 14:31:45 +0900

    yarn ng g c feature/blogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogsComponent } from './feature/blogs/blogs.component';
 import { HeaderComponent } from './feature/header/header.component';
 import { TopComponent } from './feature/top/top.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, TopComponent],
+  declarations: [AppComponent, HeaderComponent, TopComponent, BlogsComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/feature/blogs/blogs.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blogs',
+  templateUrl: './blogs.component.html',
+  styleUrls: ['./blogs.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BlogsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
