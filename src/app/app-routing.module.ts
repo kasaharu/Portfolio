bf441863c2d94063671aacf19aea0221d3ac2d85
Wataru.Kasahara <kasahara1019@gmail.com>
2019-11-04 15:02:05 +0900

    '/activities' にアクセスしたときに ActivityComponent を表示

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActivityComponent } from './feature/activity/activity.component';
 import { BlogsComponent } from './feature/blogs/blogs.component';
 import { TopComponent } from './feature/top/top.component';
 
-const routes: Routes = [{ path: '', component: TopComponent }, { path: 'blogs', component: BlogsComponent }];
+const routes: Routes = [
+  { path: '', component: TopComponent },
+  { path: 'blogs', component: BlogsComponent },
+  { path: 'activities', component: ActivityComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
