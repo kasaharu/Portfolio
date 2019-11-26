6c880655963f21b10202074d0e098ddf60f6bf23
Wataru.Kasahara <kasahara1019@gmail.com>
2019-11-27 00:24:42 +0900

    /labs への入り口を用意

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ActivityComponent } from './features/activity/ui/activity/activity.component';
 import { BlogsComponent } from './features/blogs/blogs.component';
+import { LabComponent } from './features/lab/ui/lab/lab.component';
 import { TopComponent } from './features/top/top.component';
 
 const routes: Routes = [
   { path: '', component: TopComponent },
   { path: 'blogs', component: BlogsComponent },
   { path: 'activities', component: ActivityComponent },
+  { path: 'labs', component: LabComponent },
 ];
 
 @NgModule({
