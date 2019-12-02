b008bbeeba2a0d179a63233379b2514d6abb6bfc
Wataru.Kasahara <kasahara1019@gmail.com>
2019-12-02 21:34:08 +0900

    AppComponent が Header を表示しているので declarations に登録

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './features/header/header.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      declarations: [AppComponent, HeaderComponent],
     }).compileComponents();
   }));
 
