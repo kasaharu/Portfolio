f79782aa266dd10bb302946ee11ea293dd043d66
Wataru.Kasahara <kasahara1019@gmail.com>
2019-12-02 09:21:57 +0900

    yarn ng g c shared/ui/page-title --module shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { PageTitleComponent } from './ui/page-title/page-title.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PageTitleComponent],
   imports: [
     CommonModule,
   ],
---
+++ src/app/shared/ui/page-title/page-title.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-title',
+  templateUrl: './page-title.component.html',
+  styleUrls: ['./page-title.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PageTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
