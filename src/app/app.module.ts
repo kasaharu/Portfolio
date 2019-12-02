cd9f0e92ab2ab3f444e91deef8624610fa1b29bb
Wataru.Kasahara <kasahara1019@gmail.com>
2019-12-02 09:20:42 +0900

    yarn ng g module shared --module app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { BlogsComponent } from './features/blogs/blogs.component';
 import { HeaderComponent } from './features/header/header.component';
 import { LabModule } from './features/lab/lab.module';
 import { TopComponent } from './features/top/top.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, TopComponent, BlogsComponent, ActivityComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, LabModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, LabModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
