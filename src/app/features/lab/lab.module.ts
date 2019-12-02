32dc1248838649b9e9d186bb9ed5dcf042566bf7
Wataru.Kasahara <kasahara1019@gmail.com>
2019-12-02 09:46:49 +0900

    Labs のページタイトル表示

--- src/app/features/lab/lab.module.ts
+++ src/app/features/lab/lab.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
 import { LabComponent } from './ui/lab/lab.component';
 
 @NgModule({
   declarations: [LabComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
 })
 export class LabModule {}
