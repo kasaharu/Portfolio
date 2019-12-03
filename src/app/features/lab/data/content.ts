8e4bd4b46560cda340c8c90d0a5e9dd04c63c13d
Wataru.Kasahara <kasahara1019@gmail.com>
2019-12-03 21:11:24 +0900

    /labs に表示するコンテンツデータを定義

---
+++ src/app/features/lab/data/content.ts
@@ -0,0 +1,10 @@
+import { Content } from '../domain/models';
+
+export const contents: Content[] = [
+  {
+    title: 'Redux Toolkit を試す',
+    demoPageUrl: 'https://kasaharu.github.io/try-redux-toolkit',
+    repositoryName: 'kasaharu/try-redux-toolkit',
+    repositoryUrl: 'https://github.com/kasaharu/try-redux-toolkit',
+  },
+];
