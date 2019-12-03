6052808b1e7b8ed14540faa29a80103c30ea78b7
Wataru.Kasahara <kasahara1019@gmail.com>
2019-12-03 09:33:02 +0900

    yarn ng g c shared/ui/card --module shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CardComponent } from './ui/card/card.component';
 import { PageTitleComponent } from './ui/page-title/page-title.component';
 
 @NgModule({
-  declarations: [PageTitleComponent],
+  declarations: [PageTitleComponent, CardComponent],
   imports: [CommonModule],
   exports: [PageTitleComponent],
 })
---
+++ src/app/shared/ui/card/card.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
