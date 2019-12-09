81f0a8f68513145e35aedae6af74dfeab8b01ff7
Wataru.Kasahara <kasahara1019@gmail.com>
2019-12-09 09:26:24 +0900

    yarn ng g module features/activity --module app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivityModule } from './features/activity/activity.module';
 import { ActivityComponent } from './features/activity/ui/activity/activity.component';
 import { BlogsComponent } from './features/blogs/blogs.component';
 import { HeaderComponent } from './features/header/header.component';
@@ -12,7 +13,7 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, TopComponent, BlogsComponent, ActivityComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, LabModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, LabModule, SharedModule, ActivityModule],
   providers: [],
   bootstrap: [AppComponent],
 })
