4eb33fd207b6fab2064e4f79f2fa607418bac9d6
Wataru.Kasahara <kasahara1019@gmail.com>
2019-12-09 21:32:17 +0900

    OSS 活動履歴へのリンクを追加

--- src/app/features/activity/activity-routing.module.ts
+++ src/app/features/activity/activity-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ActivityComponent } from './ui/activity/activity.component';
+import { OssListComponent } from './ui/oss-list/oss-list.component';
 
-const routes: Routes = [{ path: '', component: ActivityComponent }];
+const routes: Routes = [{ path: '', component: ActivityComponent }, { path: 'oss', component: OssListComponent }];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
