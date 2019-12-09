2c7166ac24fb0f99f3104fcfdaaf1c8cb2ad40cf
Wataru.Kasahara <kasahara1019@gmail.com>
2019-12-09 20:28:46 +0900

    ActivityComponent の h2 タグを PageTitle に変更

--- src/app/features/activity/activity.module.ts
+++ src/app/features/activity/activity.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
 import { ActivityRoutingModule } from './activity-routing.module';
 import { ActivityComponent } from './ui/activity/activity.component';
 
 @NgModule({
   declarations: [ActivityComponent],
-  imports: [CommonModule, ActivityRoutingModule],
+  imports: [CommonModule, SharedModule, ActivityRoutingModule],
 })
 export class ActivityModule {}
