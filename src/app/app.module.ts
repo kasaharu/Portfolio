3c8630315b71f25d55d0de2fb8102625727efc09
Wataru.Kasahara <kasahara1019@gmail.com>
2019-12-10 22:19:35 +0900

    Angular Material を導入したときの差分をリバート

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlogsComponent } from './features/blogs/blogs.component';
@@ -11,7 +10,7 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, TopComponent, BlogsComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, LabModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, LabModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
