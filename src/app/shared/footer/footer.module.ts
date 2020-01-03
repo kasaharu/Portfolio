5f7607695b492c44d19474643a591d6ae620f737
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-03 14:50:17 +0900

    yarn ng g c shared/footer/footer

--- src/app/shared/footer/footer.module.ts
+++ src/app/shared/footer/footer.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent],
   imports: [
     CommonModule,
   ],
