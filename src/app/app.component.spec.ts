503e24b0079fc45e5315409b312a3239212084cc
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-04 10:52:40 +0900

    AppComponent が FooterModule に依存しているので spec を修正

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,12 @@ import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './features/header/header.component';
+import { FooterModule } from './shared/footer/footer.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, FooterModule],
       declarations: [AppComponent, HeaderComponent],
     }).compileComponents();
   }));
