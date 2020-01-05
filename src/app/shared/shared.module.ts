0cf0fdffa7fede7253dd165fc2ec6b8dc7b66204
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-05 09:45:09 +0900

    yarn ng g directive shared/ui/directives/external-link --lint-fix

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CardComponent } from './ui/card/card.component';
+import { ExternalLinkDirective } from './ui/directives/external-link.directive';
 import { PageTitleComponent } from './ui/page-title/page-title.component';
 
 @NgModule({
-  declarations: [PageTitleComponent, CardComponent],
+  declarations: [PageTitleComponent, CardComponent, ExternalLinkDirective],
   imports: [CommonModule],
   exports: [PageTitleComponent, CardComponent],
 })
