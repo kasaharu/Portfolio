66ebf1421364f044d9eea0f4d1903c0edef3b268
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-05 12:14:24 +0900

    外部リンクを用意するときに必要な target, rel を付与できる directive を実装

--- src/app/shared/ui/directives/external-link.directive.spec.ts
+++ src/app/shared/ui/directives/external-link.directive.spec.ts
@@ -1,8 +1,51 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ExternalLinkDirective } from './external-link.directive';
 
-describe('ExternalLinkDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ExternalLinkDirective();
-    expect(directive).toBeTruthy();
+@Component({
+  template: '<a href="/">Test</a>',
+})
+class TestComponent {}
+
+@Component({
+  template: '<a appExternalLink href="/">Test2</a>',
+})
+class TestUsingDirectiveComponent {}
+
+describe('ExternalLinkDirective is not used', () => {
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [TestComponent, ExternalLinkDirective],
+    }).createComponent(TestComponent);
+
+    fixture.detectChanges();
+  });
+
+  it('directive を付与していない場合 debugElement が null', () => {
+    const debugElement = fixture.debugElement.query(By.directive(ExternalLinkDirective));
+    expect(debugElement).toBeNull();
+  });
+});
+
+describe('ExternalLinkDirective is used', () => {
+  let fixture: ComponentFixture<TestUsingDirectiveComponent>;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [TestUsingDirectiveComponent, ExternalLinkDirective],
+    }).createComponent(TestUsingDirectiveComponent);
+
+    fixture.detectChanges();
+  });
+
+  it('directive を付与した場合 target は _blank, rel は noopener になる', () => {
+    const debugElement = fixture.debugElement.query(By.directive(ExternalLinkDirective));
+    const element: HTMLElement = debugElement.nativeElement;
+
+    expect(element.getAttribute('target')).toBe('_blank');
+    expect(element.getAttribute('rel')).toBe('noopener');
   });
 });
--- src/app/shared/ui/directives/external-link.directive.ts
+++ src/app/shared/ui/directives/external-link.directive.ts
@@ -1,8 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
-  selector: '[appExternalLink]',
+  selector: 'a[appExternalLink]',
 })
-export class ExternalLinkDirective {
-  constructor() {}
+export class ExternalLinkDirective implements OnInit {
+  constructor(private el: ElementRef) {}
+
+  ngOnInit() {
+    const element: HTMLElement = this.el.nativeElement;
+    element.setAttribute('target', '_blank');
+    element.setAttribute('rel', 'noopener');
+  }
 }
