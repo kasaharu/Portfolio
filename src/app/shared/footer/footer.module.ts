2f4781cfbe63def979fef7aa1e97cbcf1a8afb3e
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-06 09:44:13 +0900

    外部リンクを表示する部分に ExternalLinkDirective を付与

--- src/app/shared/footer/footer.module.ts
+++ src/app/shared/footer/footer.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared.module';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [FooterComponent],
 })
 export class FooterModule {}
