c46000b65264d19b63d1766beddbf682847d9ffd
Wataru.Kasahara <kasahara1019@gmail.com>
2020-10-30 09:33:19 +0900

    yarn ng g c features/activity/ui/feed

--- src/app/features/activity/activity.module.ts
+++ src/app/features/activity/activity.module.ts
@@ -5,11 +5,12 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { SharedModule } from '../../shared/shared.module';
 import { ActivityRoutingModule } from './activity-routing.module';
 import { ActivityComponent } from './ui/activity/activity.component';
+import { FeedComponent } from './ui/feed/feed.component';
 import { OssListComponent } from './ui/oss-list/oss-list.component';
 import { SlideComponent } from './ui/slide/slide.component';
 
 @NgModule({
-  declarations: [ActivityComponent, OssListComponent, SlideComponent],
+  declarations: [ActivityComponent, OssListComponent, SlideComponent, FeedComponent],
   imports: [CommonModule, SharedModule, ActivityRoutingModule, MatTableModule, MatTabsModule],
 })
 export class ActivityModule {}
---
+++ src/app/features/activity/ui/feed/feed.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
