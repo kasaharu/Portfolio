5257094fcad54435eda1402f30e8b416ad034de4
Wataru.Kasahara <kasahara1019@gmail.com>
2020-11-12 22:29:57 +0900

    feat: Show the blog item title on activity

--- src/app/features/activity/applications/feed.service.ts
+++ src/app/features/activity/applications/feed.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import XMLParser from 'xml2js';
+import { FeedValueObject, RssResponse } from '../domain/feed';
+import { FeedStore } from '../ui/feed/feed.store';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class FeedService {
+  constructor(private componentStore: FeedStore, private readonly http: HttpClient) {}
 
-  constructor() { }
+  async fetch() {
+    const requestOptions: Object = {
+      observe: 'body',
+      responseType: 'text',
+    };
+    const result = await this.http.get<RssResponse>('https://kasaharu.hatenablog.com/rss', requestOptions).toPromise();
+    const parsed: RssResponse = await XMLParser.parseStringPromise(result);
+    const feedValueObject = FeedValueObject.createFromRssResponse(parsed);
+
+    this.componentStore.setFeed(feedValueObject.plainObject());
+  }
 }
