95a81abdb6d43362fea414170b75b6701da67777
Wataru.Kasahara <kasahara1019@gmail.com>
2020-11-15 13:37:01 +0900

    yarn ng g c features/blog/ui/blogs

--- src/app/features/blog/blog.module.ts
+++ src/app/features/blog/blog.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogPageComponent } from './pages/blog/blog.component';
+import { BlogsComponent } from './ui/blogs/blogs.component';
 
 @NgModule({
-  declarations: [BlogPageComponent],
+  declarations: [BlogPageComponent, BlogsComponent],
   imports: [CommonModule, BlogRoutingModule],
 })
 export class BlogModule {}
