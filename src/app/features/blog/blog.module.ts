9d482386421946cd49883172faff6064e32ab445
Wataru.Kasahara <kasahara1019@gmail.com>
2020-11-15 13:43:56 +0900

    yarn ng g c features/blog/containers/feed

--- src/app/features/blog/blog.module.ts
+++ src/app/features/blog/blog.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { BlogRoutingModule } from './blog-routing.module';
+import { FeedComponent } from './containers/feed/feed.component';
 import { BlogPageComponent } from './pages/blog/blog.component';
 import { BlogsComponent } from './ui/blogs/blogs.component';
 
 @NgModule({
-  declarations: [BlogPageComponent, BlogsComponent],
+  declarations: [BlogPageComponent, BlogsComponent, FeedComponent],
   imports: [CommonModule, SharedModule, BlogRoutingModule],
 })
 export class BlogModule {}
