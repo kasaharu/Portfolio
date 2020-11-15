eb8491b1d67f37d4ac06d3fee61d4b431b80c5ff
Wataru.Kasahara <kasahara1019@gmail.com>
2020-11-15 15:05:51 +0900

    fix: 不要になった activity 配下の feed を削除

--- src/app/features/activity/activity.module.ts
+++ src/app/features/activity/activity.module.ts
@@ -5,12 +5,11 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { SharedModule } from '../../shared/shared.module';
 import { ActivityRoutingModule } from './activity-routing.module';
 import { ActivityComponent } from './ui/activity/activity.component';
-import { FeedComponent } from './ui/feed/feed.component';
 import { OssListComponent } from './ui/oss-list/oss-list.component';
 import { SlideComponent } from './ui/slide/slide.component';
 
 @NgModule({
-  declarations: [ActivityComponent, OssListComponent, SlideComponent, FeedComponent],
+  declarations: [ActivityComponent, OssListComponent, SlideComponent],
   imports: [CommonModule, SharedModule, ActivityRoutingModule, MatTableModule, MatTabsModule],
 })
 export class ActivityModule {}
