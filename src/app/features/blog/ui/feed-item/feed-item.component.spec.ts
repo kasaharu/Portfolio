8542e4688993eee04c9b4da99dd6abe130c59580
Wataru.Kasahara <kasahara1019@gmail.com>
2020-11-22 12:12:46 +0900

    test: TestHostComponent を使った component のテスト

--- src/app/features/blog/ui/feed-item/feed-item.component.spec.ts
+++ src/app/features/blog/ui/feed-item/feed-item.component.spec.ts
@@ -1,24 +1,33 @@
+import { Component, DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { BlogItem } from '../../domain/feed';
 import { FeedItemComponent } from './feed-item.component';
 
+@Component({
+  template: `<app-feed-item [feedItem]="item"></app-feed-item>`,
+})
+class TestHostComponent {
+  item: BlogItem = { title: 'テストブログ', categories: [], link: '', publishDate: '' };
+}
+
 describe('FeedItemComponent', () => {
-  let component: FeedItemComponent;
-  let fixture: ComponentFixture<FeedItemComponent>;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let feedItemElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [FeedItemComponent],
+      declarations: [FeedItemComponent, TestHostComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FeedItemComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestHostComponent);
+    feedItemElement = fixture.debugElement.query(By.css('app-feed-item')).nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(feedItemElement).toBeTruthy();
   });
 });
