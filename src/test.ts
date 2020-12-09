d3d486ec3fd7f6e60b6906784ae6fefd6fd9b607
Wataru.Kasahara <kasahara1019@gmail.com>
2020-12-09 09:11:54 +0900

    chore: v11.x に合わせた細かい変更

--- src/test.ts
+++ src/test.ts
@@ -6,7 +6,12 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: any;
+declare const require: {
+  context(path: string, deep?: boolean, filter?: RegExp): {
+    keys(): string[];
+    <T>(id: string): T;
+  };
+};
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment([BrowserDynamicTestingModule, HttpClientTestingModule], platformBrowserDynamicTesting());
