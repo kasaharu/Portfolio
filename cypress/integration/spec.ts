bf636933098cb986d3b7ebc9f54f050fff8fd6d3
Wataru.Kasahara <kasahara1019@gmail.com>
2020-12-11 22:04:27 +0900

    test: Fix E2E test

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,4 +1,4 @@
 it('loads examples', () => {
   cy.visit('/');
-  cy.contains('Replace me with something relevant');
+  cy.get('app-root app-header header #headerTitle').should('contain', 'kasaharu.web');
 });
