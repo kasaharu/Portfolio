6d76f6f93b68c20196d10a83347709418a4b5460
Wataru.Kasahara <kasahara1019@gmail.com>
2020-12-17 20:24:20 +0900

    perf: Sentry の TraceService の登録

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
 import { NgWorkboxComponentsModule } from '@kasaharu/ng-workbox/components';
+import * as Sentry from '@sentry/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './features/header/header.component';
@@ -10,7 +12,6 @@ import { LabModule } from './features/lab/lab.module';
 import { TopPageComponent } from './features/top/pages/top/top.component';
 import { FooterModule } from './shared/footer/footer.module';
 import { SharedModule } from './shared/shared.module';
-import * as Sentry from '@sentry/angular';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, TopPageComponent],
@@ -24,7 +25,11 @@ import * as Sentry from '@sentry/angular';
     SharedModule,
     BrowserAnimationsModule,
   ],
-  providers: [{ provide: ErrorHandler, useValue: Sentry.createErrorHandler({ showDialog: true }) }],
+  providers: [
+    { provide: ErrorHandler, useValue: Sentry.createErrorHandler({ showDialog: true }) },
+    { provide: Sentry.TraceService, deps: [Router] },
+    { provide: APP_INITIALIZER, useFactory: () => () => {}, deps: [Sentry.TraceService], multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
