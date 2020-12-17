315b3c68e838770323f7787c585e6d36357dfa2f
Wataru.Kasahara <kasahara1019@gmail.com>
2020-12-17 20:02:56 +0900

    perf: Sentry の導入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgWorkboxComponentsModule } from '@kasaharu/ng-workbox/components';
@@ -10,6 +10,7 @@ import { LabModule } from './features/lab/lab.module';
 import { TopPageComponent } from './features/top/pages/top/top.component';
 import { FooterModule } from './shared/footer/footer.module';
 import { SharedModule } from './shared/shared.module';
+import * as Sentry from '@sentry/angular';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, TopPageComponent],
@@ -23,7 +24,7 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useValue: Sentry.createErrorHandler({ showDialog: true }) }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import * as Sentry from '@sentry/angular';
+import { Integrations } from '@sentry/tracing';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+Sentry.init({
+  dsn: 'https://a64ddf4e943f4e37a318779ebc1108b6@o492848.ingest.sentry.io/5560941',
+  environment: environment.production ? 'production' : 'development',
+  integrations: [
+    // Registers and configures the Tracing integration,
+    // which automatically instruments your application to monitor its
+    // performance, including custom Angular routing instrumentation
+    new Integrations.BrowserTracing({
+      tracingOrigins: ['localhost'],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 if (environment.production) {
   enableProdMode();
