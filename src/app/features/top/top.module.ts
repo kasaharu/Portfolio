158e2fc721fe23933e163b97d436d8533eefc856
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-17 09:29:21 +0900

    yarn ng g module features/top --routing

---
+++ src/app/features/top/top.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { TopRoutingModule } from './top-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TopRoutingModule,
+  ],
+})
+export class TopModule { }
