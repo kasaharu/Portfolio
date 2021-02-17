7aa84099fc0901833629f56df1aa7d76cd419a4b
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-17 21:02:26 +0900

    yarn ng g c features/top/containers/top

--- src/app/features/top/top.module.ts
+++ src/app/features/top/top.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
+import { TopComponent } from './containers/top/top.component';
 import { TopPageComponent } from './pages/top/top.component';
 import { TopRoutingModule } from './top-routing.module';
 
 @NgModule({
-  declarations: [TopPageComponent],
+  declarations: [TopPageComponent, TopComponent],
   imports: [CommonModule, TopRoutingModule, SharedModule],
 })
 export class TopModule {}
