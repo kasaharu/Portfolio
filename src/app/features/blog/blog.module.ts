f336652d2de277fb7a77dd3ca9cde755ff74d9df
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-24 20:32:23 +0900

    refactor(blog): 移行した FeedComponent を削除

--- src/app/features/blog/blog.module.ts
+++ src/app/features/blog/blog.module.ts
@@ -4,13 +4,12 @@ import { ExternalLinkModule } from '../../shared/external-link/external-link.mod
 import { PageTitleModule } from '../../shared/page-title/page-title.module';
 import { BlogRoutingModule } from './blog-routing.module';
 import { ArticleComponent } from './containers/article/article.component';
-import { FeedComponent } from './containers/feed/feed.component';
 import { BlogPageComponent } from './pages/blog/blog.component';
 import { BlogsComponent } from './ui/blogs/blogs.component';
 import { FeedItemComponent } from './ui/feed-item/feed-item.component';
 
 @NgModule({
-  declarations: [BlogPageComponent, BlogsComponent, FeedComponent, FeedItemComponent, ArticleComponent],
+  declarations: [BlogPageComponent, BlogsComponent, FeedItemComponent, ArticleComponent],
   imports: [CommonModule, BlogRoutingModule, PageTitleModule, ExternalLinkModule],
 })
 export class BlogModule {}
