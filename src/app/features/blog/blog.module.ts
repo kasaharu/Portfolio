837375ca157cd25cc9e994475e8e9e521ac0dd62
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-24 09:03:58 +0900

    yarn ng g c features/blog/containers/article

--- src/app/features/blog/blog.module.ts
+++ src/app/features/blog/blog.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { ExternalLinkModule } from '../../shared/external-link/external-link.module';
 import { PageTitleModule } from '../../shared/page-title/page-title.module';
 import { BlogRoutingModule } from './blog-routing.module';
+import { ArticleComponent } from './containers/article/article.component';
 import { FeedComponent } from './containers/feed/feed.component';
 import { BlogPageComponent } from './pages/blog/blog.component';
 import { BlogsComponent } from './ui/blogs/blogs.component';
 import { FeedItemComponent } from './ui/feed-item/feed-item.component';
 
 @NgModule({
-  declarations: [BlogPageComponent, BlogsComponent, FeedComponent, FeedItemComponent],
+  declarations: [BlogPageComponent, BlogsComponent, FeedComponent, FeedItemComponent, ArticleComponent],
   imports: [CommonModule, BlogRoutingModule, PageTitleModule, ExternalLinkModule],
 })
 export class BlogModule {}
---
+++ src/app/features/blog/containers/article/article.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
