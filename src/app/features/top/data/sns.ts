737e4e7fc78537f4ef88c61c351285cdb5641207
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-28 15:24:48 +0900

    refactor(top): ページにリンクを追加

--- src/app/features/top/data/sns.ts
+++ src/app/features/top/data/sns.ts
@@ -4,4 +4,7 @@ export const snsList: Sns[] = [
   { serviceName: 'GitHub', url: 'https://github.com/kasaharu' },
   { serviceName: 'Twitter', url: 'https://twitter.com/kasaharu' },
   { serviceName: 'connpass', url: 'https://connpass.com/user/kasaharu/' },
+  { serviceName: 'はてなブログ', url: 'https://kasaharu.hatenablog.com/' },
+  { serviceName: 'Medium', url: 'https://medium.com/@kasaharu' },
+  { serviceName: 'Qiita', url: 'https://qiita.com/kasaharu' },
 ];
