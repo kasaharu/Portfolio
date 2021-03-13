78a269a272bff7ad2f05c123aec1300828bce976
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-13 18:40:05 +0900

    perf: assets size が大きくなるため ngneat/inspector を削除

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { InspectorModule } from '@ngneat/inspector';
 import * as Sentry from '@sentry/angular';
-import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    CoreModule,
-    environment.production ? [] : InspectorModule.forRoot({ position: { top: 'auto', bottom: '20px' } }),
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CoreModule],
   providers: [
     { provide: ErrorHandler, useValue: Sentry.createErrorHandler({ showDialog: true }) },
     { provide: Sentry.TraceService, deps: [Router] },
