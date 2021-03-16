4957803d52e33b1da555a046fad3d1099b970f7f
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-16 20:34:24 +0900

    fix(activity): 日付のフォーマットを変更

--- src/app/features/activity/data/slide.ts
+++ src/app/features/activity/data/slide.ts
@@ -2,7 +2,7 @@ import { Slide } from '../domain/models';
 
 export const slideList: Slide[] = [
   {
-    date: '2020-10-17',
+    date: '2020.10.17',
     event: { name: 'GDG DevFest 2020 / mini ng-japan 2020', url: 'https://gdg-tokyo.connpass.com/event/190079/' },
     slide: {
       name: 'NgRx component と component-store について',
@@ -10,7 +10,7 @@ export const slideList: Slide[] = [
     },
   },
   {
-    date: '2019-10-03',
+    date: '2019.10.03',
     event: { name: 'Classi Angular Night #4', url: 'https://classi-angular-night.connpass.com/event/144066/' },
     slide: {
       name: 'Classi リプレイスプロジェクト フロントエンドアーキテクチャ変遷',
@@ -18,17 +18,17 @@ export const slideList: Slide[] = [
     },
   },
   {
-    date: '2019-06-13',
+    date: '2019.06.13',
     event: { name: 'Classi Angular Night #3', url: 'https://classi-angular-night.connpass.com/event/131195/' },
     slide: { name: 'Deep dive into component', url: 'https://speakerdeck.com/kasaharu/classi-angular-night-number-3' },
   },
   {
-    date: '2019-03-27',
+    date: '2019.03.27',
     event: { name: 'Classi Angular Night #2', url: 'https://classi-angular-night.connpass.com/event/121367/' },
     slide: { name: 'Angular Way をまっすぐ歩くために', url: 'https://speakerdeck.com/kasaharu/classi-angular-night-number-2' },
   },
   {
-    date: '2019-01-31',
+    date: '2019.01.31',
     event: { name: 'Classi Angular Night #1', url: 'https://classi-angular-night.connpass.com/event/112186/' },
     slide: {
       name: 'リプレイスプロジェクトで考えるフロントエンド開発',
@@ -36,7 +36,7 @@ export const slideList: Slide[] = [
     },
   },
   {
-    date: '2016-12-19',
+    date: '2016.12.19',
     event: { name: '【React.js】LT忘年会', url: 'https://teamspirit.connpass.com/event/46336/' },
     slide: {
       name: 'React 初心者が初級者になるまで',
