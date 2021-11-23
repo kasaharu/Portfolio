8e57e19dba6bf433c7c91065024b10b67a83b364
Wataru.Kasahara <kasahara1019@gmail.com>
2021-11-23 19:56:32 +0900

    test: Jest 用の設定ファイルの更新

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
+require('jest-preset-angular/ngcc-jest-processor');
+
 module.exports = {
-  moduleNameMapper: {
-    '@core/(.*)': '<rootDir>/src/app/core/$1',
-  },
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testMatch: ["<rootDir>/src/**/*.spec.ts"],
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
 };
