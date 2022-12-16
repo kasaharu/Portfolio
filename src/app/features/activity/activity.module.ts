806c1dba532f75dbf69c3480180e6fa5445cef1b
Wataru.Kasahara <kasahara1019@gmail.com>
2022-12-16 21:24:42 +0900

    yarn ng update @angular/cli @angular/core @angular-eslint/schematics @angular/cdk @angular/material: v14.2.9 -> v15.0.4

--- src/app/features/activity/activity.module.ts
+++ src/app/features/activity/activity.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
-import { MatTabsModule } from '@angular/material/tabs';
+import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table';
+import { MatLegacyTabsModule as MatTabsModule } from '@angular/material/legacy-tabs';
 import { ExternalLinkModule } from '../../shared/external-link/external-link.module';
 import { PageTitleModule } from '../../shared/page-title/page-title.module';
 import { ActivityRoutingModule } from './activity-routing.module';
