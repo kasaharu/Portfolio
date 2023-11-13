57846e4a276d8aa7153bb42f58f61edeac8c8410
Wataru.Kasahara <kasahara1019@gmail.com>
2023-11-13 21:45:37 +0900

    chore: Sentry の設定を外す

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,28 @@
-import { APP_INITIALIZER, enableProdMode, ErrorHandler, importProvidersFrom } from '@angular/core';
-import * as Sentry from '@sentry/angular';
-import { Integrations } from '@sentry/tracing';
-
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
-import { Router } from '@angular/router';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
-Sentry.init({
-  dsn: 'https://a64ddf4e943f4e37a318779ebc1108b6@o492848.ingest.sentry.io/5560941',
-  environment: environment.production ? 'production' : 'development',
-  integrations: [
-    // Registers and configures the Tracing integration,
-    // which automatically instruments your application to monitor its
-    // performance, including custom Angular routing instrumentation
-    new Integrations.BrowserTracing({
-      tracingOrigins: ['localhost'],
-      routingInstrumentation: Sentry.routingInstrumentation,
-    }),
-  ],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+// Sentry.init({
+//   dsn: 'https://a64ddf4e943f4e37a318779ebc1108b6@o492848.ingest.sentry.io/5560941',
+//   environment: environment.production ? 'production' : 'development',
+//   integrations: [
+//     // Registers and configures the Tracing integration,
+//     // which automatically instruments your application to monitor its
+//     // performance, including custom Angular routing instrumentation
+//     new Integrations.BrowserTracing({
+//       tracingOrigins: ['localhost'],
+//       routingInstrumentation: Sentry.routingInstrumentation,
+//     }),
+//   ],
+//
+//   // Set tracesSampleRate to 1.0 to capture 100%
+//   // of transactions for performance monitoring.
+//   // We recommend adjusting this value in production
+//   tracesSampleRate: 1.0,
+// });
 
 if (environment.production) {
   enableProdMode();
@@ -35,9 +31,9 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserModule, AppRoutingModule),
-    { provide: ErrorHandler, useValue: Sentry.createErrorHandler({ showDialog: true }) },
-    { provide: Sentry.TraceService, deps: [Router] },
-    { provide: APP_INITIALIZER, useFactory: () => () => {}, deps: [Sentry.TraceService], multi: true },
+    // { provide: ErrorHandler, useValue: Sentry.createErrorHandler({ showDialog: true }) },
+    // { provide: Sentry.TraceService, deps: [Router] },
+    // { provide: APP_INITIALIZER, useFactory: () => () => {}, deps: [Sentry.TraceService], multi: true },
     provideHttpClient(withInterceptorsFromDi()),
   ],
 }).catch((err) => console.error(err));
