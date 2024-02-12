e6c974a0b3f7cf3b2086043e5c59702acd9f17b3
Wataru.Kasahara <kasahara1019@gmail.com>
2024-02-12 13:14:05 +0900

    refactor: feature module を削除して直接 component をルーティングに指定

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+import { TopPageComponent } from './features/top/pages/top/top.component';
 
 export const routes: Routes = [
-  { path: '', loadChildren: () => import('./features/top/top.module').then((m) => m.TopModule) },
+  // { path: '', loadChildren: () => import('./features/top/top.module').then((m) => m.TopModule) },
+  { path: '', component: TopPageComponent },
   { path: 'blogs', loadChildren: () => import('./features/blog/blog.module').then((m) => m.BlogModule) },
   { path: 'activities', loadChildren: () => import('./features/activity/activity.module').then((m) => m.ActivityModule) },
   { path: 'labs', loadChildren: () => import('./features/lab/lab.module').then((m) => m.LabModule) },
