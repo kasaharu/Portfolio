a1e53caedd23ef7f0140d5538ce48c2369462a4c
Wataru.Kasahara <kasahara1019@gmail.com>
2024-09-17 12:41:37 +0900

    refactor: ComponentStore ベースの ArticleStore を 削除

--- src/app/features/blog/containers/articles/article.store.ts
+++ src/app/features/blog/containers/articles/article.store.ts
@@ -1,21 +1,5 @@
-import { Injectable } from '@angular/core';
-import { ComponentStore } from '@ngrx/component-store';
 import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
-import { Observable } from 'rxjs';
 import { Article, Feed } from '../../domain/feed';
-
-@Injectable()
-export class DeprecatedArticleStore extends ComponentStore<Feed> {
-  constructor() {
-    super({ items: [] });
-  }
-
-  readonly blogItems$: Observable<Article[]> = this.select((state) => state.items);
-
-  readonly setFeed = this.updater((_, feed: Feed) => {
-    return { items: feed.items };
-  });
-}
 
 type ArticleState = {
   items: Article[];
