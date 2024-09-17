395ef548cbd592498564e1ead5cbca1106a008b0
Wataru.Kasahara <kasahara1019@gmail.com>
2024-09-17 11:53:46 +0900

    refactor: blog feature の NgModule を削除

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { TopPageComponent } from './features/top/top.component';
 export const routes: Routes = [
   // { path: '', loadChildren: () => import('./features/top/top.module').then((m) => m.TopModule) },
   { path: '', component: TopPageComponent },
-  { path: 'blogs', loadChildren: () => import('./features/blog/blog.module').then((m) => m.BlogModule) },
+  { path: 'blogs', loadComponent: () => import('./features/blog/pages/blog/blog.component').then((m) => m.BlogPageComponent) },
   { path: 'activities', loadChildren: () => import('./features/activity/activity.module').then((m) => m.ActivityModule) },
   { path: 'labs', loadChildren: () => import('./features/lab/lab.module').then((m) => m.LabModule) },
 ];
